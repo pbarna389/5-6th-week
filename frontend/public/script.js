663dff920f8d3136ca14b25c7a5f3794cb2e75ae
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-05-02 09:50:13 +0200

    basic elements added

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,48 +1,40 @@
-console.table(beers.cards);
-
-const beerTitleComponent = () => {
+const headerComponent = (title) => {
     return `
-    <h1>Beers</h1>
+    <header>${title}</header>
     `
 };
 
-const beerSectionComponent = (innerData, id = `beer-0`) => {
+const sectionComponent = (id, button, h2text) => {
     return `
-        <section id=${id}>
-            ${innerData}
-        </section>`
-};
-
-const beerCardComponent = (name, comp, type) => {
-    return `
-        <div>
-            <h1>Name: ${name}</h1>
-            <h3>Company: ${comp}</h3>
-            <h4>Type: ${type}</h4>
-        </div>
+    <section id="${id}">
+        <h2>${h2text}</h2>
+        <button>${button}</button>
+    </section>
     `
 };
 
-const init = () => {
+const footerComponent = () => {
+    return `
+        <footer></footer>
+    `
+};
+
+const loadEvent = () => {
     const rootElement = document.querySelector('#root');
-    const beerCards = beers.cards;
 
-    rootElement.insertAdjacentHTML('beforebegin', beerTitleComponent());
+    rootElement.insertAdjacentHTML('beforebegin', headerComponent('Responsivity practice'));
 
-    for (const element of beerCards) {
-        // let beerData = "";
-        // let beerID = "";
-
-        // beerData += beerCardComponent(element.title, element.sub, element.text);
-        // beerID += `beer-0${beerCards.indexOf(element) + 1}`;
-
-        rootElement.insertAdjacentHTML('beforeend', beerSectionComponent(beerCardComponent(element.title, element.sub, element.text), `beer-0${beerCards.indexOf(element) + 1}`));
+    let sections = "";
+    for (let i = 1; i <= 4; i++) {
+        sections += sectionComponent(`id-0${i}`, `button-0${i}`, `Subtitle-0${i}`);
     };
+
+    rootElement.insertAdjacentHTML('beforebegin', sections);
+
+    rootElement.insertAdjacentHTML('afterend', footerComponent());
+
+    console.log(sections);
+
 };
 
-// A JS létrehoz egy window objektumot, amit a böngésző detektál
-// addEventListener => az eseményfigyelő
-window.addEventListener("load", init);
-
-//Feladatok: nyomkodni a for ciklusokat ezerrel
-//Extra feladat: kapunk egy ugyanilyen adatstruktúrájú js file-t, egy tök új repoba ugyanezeket meg kell csinálni -> mozifilmes 
+window.addEventListener("load", loadEvent);
